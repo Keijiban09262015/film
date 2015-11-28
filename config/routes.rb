97243d3b1414169c9154fbc840f18d1977d94501
chun_liang <wumegawa1020@gamil.com>
2015-11-28 21:43:55 +0900

    add sample view test.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   get "types/show"
 
+  get "types/test"
+
   root "types#index"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
