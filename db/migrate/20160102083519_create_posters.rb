c90c4a7b62d0c772dace4d75ca9f9873df7a25c8
chun_liang <wumegawa1020@gamil.com>
2016-01-02 18:57:43 +0900

    modify poster model

--- db/migrate/20160102083519_create_posters.rb
+++ db/migrate/20160102083519_create_posters.rb
@@ -1,7 +1,7 @@
 class CreatePosters < ActiveRecord::Migration
   def change
     create_table :posters do |t|
-      t.string :title_id, :null => false
+      t.string :title_id, :null => false, :limit => 5
       t.string :img_link, :null => false, :limit => 200
 
       t.timestamps null: false
